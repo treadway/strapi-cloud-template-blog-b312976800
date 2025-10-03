fba7b10071779adb07e6b67d44c02796e1a30c2e
Chris Treadway <chris.treadway@radiant.digital>
2025-10-02 19:07:22 -0600

    Strapi Auth Fixes / Logging

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,33 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+	"strapi::logger",
+	"strapi::errors",
+	{
+		name: "strapi::security",
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					"connect-src": ["'self'", "https:"],
+					"img-src": ["'self'", "data:", "blob:", "https:"],
+					"media-src": ["'self'", "data:", "blob:", "https:"],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
+	{
+		name: "strapi::cors",
+		config: {
+			enabled: true,
+			origin: ["*"], // For testing - restrict this later
+		},
+	},
+	"strapi::security",
+	"strapi::cors",
+	"strapi::poweredBy",
+	"strapi::query",
+	"strapi::body",
+	"strapi::session",
+	"strapi::favicon",
+	"strapi::public",
 ];
---
+++ src/extensions/users-permissions/server/index.js
@@ -0,0 +1,7 @@
+const controllers = require("./controllers");
+const routes = require("./routes");
+
+module.exports = {
+	controllers,
+	routes,
+};
