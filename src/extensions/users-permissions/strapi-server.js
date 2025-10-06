ab48cc41d350d56ba3b8abb15ae3004262c0fc9f
Chris Treadway <chris.treadway@radiant.digital>
2025-10-06 13:57:30 -0600

    Strapi 5 user-permissison strapi-server.js fix

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,1 +1,19 @@
-module.exports = require("./server");
+export default (plugin) => {
+	// --- keep or merge your custom routes/controllers ---
+	const customControllers = require("./server/controllers");
+	const customRoutes = require("./server/routes");
+
+	// Merge controllers
+	plugin.controllers = {
+		...plugin.controllers,
+		...customControllers,
+	};
+
+	// Merge content-api routes
+	plugin.routes["content-api"].routes = [
+		...plugin.routes["content-api"].routes,
+		...customRoutes["content-api"].routes,
+	];
+
+	return plugin;
+};
