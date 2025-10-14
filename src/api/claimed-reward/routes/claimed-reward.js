9063048ea94fbed6d35726ccd6803f717904f45e
Chris Treadway <chris.treadway@radiant.digital>
2025-10-14 12:58:21 -0600

    Badd Auth Object in Caimed-Reward

--- src/api/claimed-reward/routes/claimed-reward.js
+++ src/api/claimed-reward/routes/claimed-reward.js
@@ -37,7 +37,7 @@ module.exports = {
 			method: "GET",
 			path: "/claimed-rewards/:id/pass",
 			handler: "claimed-reward.generatePass",
-			config: { auth: false },
+			auth: false,
 		},
 	],
 };
