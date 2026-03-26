2cbce33eb65262d5268f11a47dee2e580a82e7d2
Chris Treadway <chris.treadway@radiant.digital>
2026-03-25 21:13:56 -0600

    Chris is always right

--- src/api/business/controllers/business.js
+++ src/api/business/controllers/business.js
@@ -5,13 +5,40 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController(
 	"api::business.business",
 	({ strapi }) => ({
+		async find(ctx) {
+			// Always populate logo, photo, and linked rewards with their image
+			ctx.query = {
+				...ctx.query,
+				populate: {
+					logo: true,
+					photo: true,
+					rewards: {
+						populate: { image: true },
+					},
+				},
+			};
+			return super.find(ctx);
+		},
+
+		async findOne(ctx) {
+			// Same populate for single business lookup
+			ctx.query = {
+				...ctx.query,
+				populate: {
+					logo: true,
+					photo: true,
+					rewards: {
+						populate: { image: true },
+					},
+				},
+			};
+			return super.findOne(ctx);
+		},
+
 		async create(ctx) {
 			// Set owner to current user
 			ctx.request.body.data.owner = ctx.state.user.id;
-
-			// Call default create
-			const response = await super.create(ctx);
-			return response;
+			return super.create(ctx);
 		},
 	})
 );
