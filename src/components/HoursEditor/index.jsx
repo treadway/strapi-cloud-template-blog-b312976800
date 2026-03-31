ab3cb3aa11751138c2e034a299278b27f6cfb4c6
Chris Treadway <chris.treadway@radiant.digital>
2026-03-31 17:54:43 -0600

    Test Box

--- src/components/HoursEditor/index.jsx
+++ src/components/HoursEditor/index.jsx
@@ -27,37 +27,8 @@ const HoursEditor = ({ value = {}, onChange, name }) => {
 		<div>
 			{days.map((day) => {
 				const data = value[day] || {};
-
 				return (
-					<div key={day} style={{ marginBottom: 12 }}>
-						<strong>{day}</strong>
-
-						<div style={{ display: "flex", gap: 8 }}>
-							<label>
-								Closed
-								<input
-									type="checkbox"
-									checked={data.closed || false}
-									onChange={(e) => updateDay(day, "closed", e.target.checked)}
-								/>
-							</label>
-
-							{!data.closed && (
-								<>
-									<input
-										type="time"
-										value={data.open || ""}
-										onChange={(e) => updateDay(day, "open", e.target.value)}
-									/>
-									<input
-										type="time"
-										value={data.close || ""}
-										onChange={(e) => updateDay(day, "close", e.target.value)}
-									/>
-								</>
-							)}
-						</div>
-					</div>
+					<div style={{ padding: 20, background: "red" }}>HOURS EDITOR</div>
 				);
 			})}
 		</div>
