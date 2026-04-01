94dae8a31a7bb11722df3a108f75610248404157
Chris Treadway <chris.treadway@radiant.digital>
2026-04-01 11:39:38 -0600

    Hours Fix?

--- src/admin/app.js
+++ src/admin/app.js
@@ -6,10 +6,17 @@ export default {
 			name: "hours-editor",
 			pluginId: "global",
 			type: "json",
+
 			intlLabel: {
 				id: "hours-editor.label",
 				defaultMessage: "Business Hours",
 			},
+
+			intlDescription: {
+				id: "hours-editor.description",
+				defaultMessage: "Set weekly business hours",
+			},
+
 			components: {
 				Input: HoursEditor,
 			},
