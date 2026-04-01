d6e3f3fff32c90a4e747f2df2715b161b4d25af5
Chris Treadway <chris.treadway@radiant.digital>
2026-04-01 12:33:29 -0600

    Temp COntroller repleacement REVERT!

--- src/api/business/controllers/business.js
+++ src/api/business/controllers/business.js
@@ -2,39 +2,4 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController(
-	"api::business.business",
-	({ strapi }) => ({
-		async find(ctx) {
-			return await strapi.service("api::business.business").find({
-				...ctx.query,
-				populate: {
-					logo: true,
-					photo: true,
-					rewards: {
-						populate: { image: true },
-					},
-				},
-			});
-		},
-
-		async findOne(ctx) {
-			const { id } = ctx.params;
-
-			return await strapi.service("api::business.business").findOne(id, {
-				populate: {
-					logo: true,
-					photo: true,
-					rewards: {
-						populate: { image: true },
-					},
-				},
-			});
-		},
-
-		async create(ctx) {
-			ctx.request.body.data.owner = ctx.state.user.id;
-			return super.create(ctx);
-		},
-	})
-);
+module.exports = createCoreController("api::business.business");
